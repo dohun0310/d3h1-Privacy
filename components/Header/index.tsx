b638faccf239af84d0ff6f3146081019a9add1f8
김도훈 <dohun0310@naver.com>
2025-12-30 14:48:48 +0900

    Refactor: `Header` 컴포넌트에서 CSS 모듈 제거 및 Tailwind CSS 클래스 적용

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,19 +1,25 @@
-import styles from "./header.module.css";
+"use client"
+
 import Link from "next/link";
+import Logo from "../Logo";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <Link href="/">
-          <svg width="168" height="300" viewBox="0 0 168 300" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0.224854L166.417 43.1784H0V0.224854Z"/>
-            <path d="M68.8488 300L3.76701e-05 -8.9407e-06L3.76701e-05 300H68.8488Z"/>
-            <path d="M116.717 43.1785L165.967 258.171L165.967 43.1785L116.717 43.1785Z"/>
-            <path d="M167.991 260.977L73.6323 300H0V260.977H167.991Z"/>
-          </svg>
+    <header className="w-full h-15 lg:h-16 top-0 left-0
+      fixed z-20 bg-background
+      border-b border-gray-100 dark:border-gray-800"
+    >
+      <div className="w-full max-w-[1325px] h-full
+        relative mx-auto px-4
+        flex items-center justify-between"
+      >
+        <Link
+          href="/"
+          aria-label="logo"
+        >
+          <Logo size={36} />
         </Link>
       </div>
     </header>
-  )
+  );
 }
