9f0e2559bcf0506c2c523469a2ec77a3e7f3660b
김도훈 <dohun0310@naver.com>
2026-07-06 18:10:18 +0900

    [Chore/#3] Feat: `npx @opennextjs/cloudflare migrate`실행

--- next.config.ts
+++ next.config.ts
@@ -2,3 +2,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
